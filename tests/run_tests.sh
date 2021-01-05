3d3ec293abaa6c8a7dcc8f4cd0ca71f526e9ea28
Sören Tempel <tempel@uni-bremen.de>
2021-01-05 12:49:55 +0100

    tests: change exit status in run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,15 +4,19 @@ set -e
 # Change to directory of script.
 cd "${0%/*}"
 
+retval=0
 find . -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) | while read f; do
 	[ "${f##*/}" = "${0##*/}" ] && continue
 	printf "Running test case %s: " "${f##*/}"
 
 	reg=$(tiny32-vp --intercept-syscalls "${f}" 2>/dev/null | awk '/^a6/ { print $4 }')
 	if [ "${reg}" -ne 2342 ]; then
-		printf "FAIL [expected %d - got %d]" "2342" "${reg}"
-	else
-		printf "OK"
+		printf "FAIL [expected %d - got %d]\n" "2342" "${reg}\n"
+		retval=1
+		continue
 	fi
-	printf "\n"
+
+	printf "OK\n"
 done
+
+exit ${retval}
