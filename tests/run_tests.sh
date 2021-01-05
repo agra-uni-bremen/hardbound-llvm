7f4c92ed282fb3b057c3a1a7e06936ef770b0d94
Sören Tempel <tempel@uni-bremen.de>
2021-01-05 01:48:31 +0100

    tests: add script to run test cases

---
+++ tests/run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# Change to directory of script.
+cd "${0%/*}"
+
+find . -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) | while read f; do
+	[ "${f##*/}" = "${0##*/}" ] && continue
+	printf "Running test case %s: " "${f##*/}"
+
+	reg=$(tiny32-vp --intercept-syscalls "${f}" 2>/dev/null | awk '/^a6/ { print $4 }')
+	if [ "${reg}" -ne 2342 ]; then
+		printf "FAIL [expected %d - got %d]" "2342" "${reg}"
+	else
+		printf "OK"
+	fi
+	printf "\n"
+done
