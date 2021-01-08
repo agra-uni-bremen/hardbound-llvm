85e77ebc718bc791a2fd1d29cc2c44ab57944a5a
Sören Tempel <tempel@uni-bremen.de>
2021-01-08 11:15:29 +0100

    tests: Document how buffer-subscript-bounds must be handled

--- tests/wip/buffer-subscript-bounds.c
+++ tests/wip/buffer-subscript-bounds.c
@@ -7,3 +7,18 @@ main(void)
 
 	return *(intptr);
 }
+
+/*
+Must be rewritten tto:
+
+int
+main(void)
+{
+	int buf[5];
+	int *bufptr = &buf[0];
+	unsigned int i = 5;
+	int *intptr = bufptr + i;
+
+	return *(intptr);
+}
+*/
